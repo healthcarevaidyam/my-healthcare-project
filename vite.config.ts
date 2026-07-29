97e122aeacde5c9ed5b0b43f7b8639ace88de75a
Prafull Sharma <Prafullsharma0524@gmail.com>
2026-07-30 01:24:54 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: "/",
+  base: "/",
   server: {
-    host: "::",
-    port: 8080,
+    host: "localhost",
+    port: 5173,
+    strictPort: true,
     hmr: {
       overlay: false,
     },

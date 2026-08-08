e0effdf5e9250de32d13689dfb050352cbaaebc7
Prafull Sharma <Prafullsharma0524@gmail.com>
2026-08-08 20:03:00 +0530

    Add AI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar = () => {
 
         <div>
           <span className="font-serif text-[10px] font-semibold uppercase tracking-[0.4em] text-black-700">
-          BALKRISHNA
+          BALKRISHNA's
         </span>
 
           <h1 className="font-heading text-3xl font-bold text-slate-800">

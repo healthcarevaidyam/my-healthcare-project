bbfef0e20729a1b9fae860097b40d118d39e2072
Prafull Sharma <Prafullsharma0524@gmail.com>
2026-08-20 11:25:12 +0530

    Doctor Details

--- src/components/admin/doctorpad/types.ts
+++ src/components/admin/doctorpad/types.ts
@@ -2,16 +2,23 @@
 
 export interface Prescription {
   patientName: string;
+
   age: string;
+
   sex: string;
+
   date: string;
+
   fee: string;
 
   consultationType: string;
 
   bp: string;
+
   temperature: string;
+
   pulse: string;
+
   oxygen: string;
 
   doctorNotes: string;
